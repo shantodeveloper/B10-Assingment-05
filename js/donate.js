149d5681cd404c0bf6ca5e2ebe8b866fc773b2ad
shantodeveloper <ahmedshanto690@gmail.com>
2024-09-24 11:25:39 -0700

    histori added functionality

--- js/donate.js
+++ js/donate.js
@@ -1,5 +1,6 @@
 function handleClick(titel, input, balance) {
-  const titleElement = document.getElementById(titel);
+  const titleElement = document.getElementById(titel).innerText;
+
   const inputAmounts = getInputAmount(input);
   const innerbalance = getInnerAmount(balance);
   const accountbalance = getInnerAmount('acount-balance');
@@ -8,5 +9,11 @@ function handleClick(titel, input, balance) {
   countbalancedescris = accountbalance - inputAmounts;
   document.getElementById('acount-balance').innerText = countbalancedescris;
   document.getElementById(balance).innerText = totalbalance;
+
+  const historyadd = document.getElementById('history-card');
+
+  const history = document.createElement('div');
+  history.innerHTML = `<h1 class="font-bold mb-4 border p-8">${inputAmounts} Taka is Donate for ${titleElement}</h1> `;
+  historyadd.appendChild(history);
   document.getElementById('my_modal_6').checked = true;
 }
